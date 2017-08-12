5c09b6248cd2e3f0b27ee58b90daff10e0811814
root <root@localhost.localdomain>
2017-08-12 10:09:33 -0500

    Adding IntrusionDetection LR

---
+++ AnomalyDetection/IDS.py
@@ -0,0 +1,24 @@
+## Procesar el KDD 2015 Intrusion Detection from UNB 
+#!/usr/bin/env python
+import tensorflow as tf
+msg = tf.constant("Test Dataset TF")
+sess = tf.Session()
+print(sess.run(msg))
+x = tf.constant(9)
+y = tf.constant(2)
+print(sess.run(x + y))
+import pandas as pn
+pn.set_option('display.max_columns', 500)
+import zipfile
+with zipfile.ZipFile('../datasets/UNB_ISCX_NSL_KDD.csv.zip', 'r') as z:
+    f = z.open('UNB_ISCX_NSL_KDD.csv')
+    data = pn.io.parsers.read_table(f, sep=',')
+data.head()
+print(data)
+y = (data['class'] == 'anomaly').astype(int)
+print(y)
+y.value_counts()
+X = data[['same_srv_rate','dst_host_srv_count']]
+print(X)
+y = (data['class'] == 'anomaly').astype(int)
+print(y)
